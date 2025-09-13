15d2b622af7272a754b3be16071e07eabdb5f46c
sameertrimade <sameertrimade21@gmail.com>
2025-09-14 01:33:18 +0530

    feat(login): design layout for login form using Angular Material

---
+++ src/app/shared/feather-icon.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
+import feather from 'feather-icons';
+
+type FeatherIconName = keyof typeof feather.icons;
+
+@Directive({
+  selector: '[appFeatherIcon]',
+  standalone: true,
+})
+export class FeatherIconDirective implements OnChanges {
+  @Input('appFeatherIcon') iconName!: FeatherIconName;
+  @Input() width?: number;
+  @Input() height?: number;
+
+  constructor(private el: ElementRef<HTMLElement>) {}
+
+  ngOnChanges(): void {
+    const icon = feather.icons[this.iconName];
+    if (icon)
+      this.el.nativeElement.innerHTML = icon.toSvg({
+        width: this.width ?? 24,
+        height: this.height ?? 24,
+      });
+  }
+}
