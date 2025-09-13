03f3b45815154113dfd4ce24d40fd34e12ccc43e
sameertrimade <sameertrimade21@gmail.com>
2025-09-13 19:05:26 +0530

    feat: add Dashboard, Login, List, NotFound components with routing and husky integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LogInComponent } from './auth/login/login';
+import { DashboardComponent } from './dashboard/dashboard';
+import { ListComponent } from './list/list';
+import { NotFoundComponent } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LogInComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'list', component: ListComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
---
+++ src/app/list/list.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-list',
+  templateUrl: './list.html',
+  styleUrl: './list.scss',
+})
+export class ListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
